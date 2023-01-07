676497c604fcda0aa7fae3dfe6ec0e67f8fa4ec9
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2023-01-07 16:04:27 +0000

    add initial winget install script

---
+++ run-winget-install.ps1
@@ -0,0 +1,55 @@
+# Entertaiment
+#
+
+winget install --id=Plex.PlexMediaPlayer -e ;
+
+
+# Security
+#
+
+winget install --id=Bitwarden.Bitwarden -e;
+winget install --id=NordVPN.NordVPN -e;
+winget install --id=Twilio.Authy -e;
+
+
+# Development
+#
+
+winget install --id=AdoptOpenJDK.OpenJDK.17 -e;
+winget install --id=Figma.Figma -e;
+winget install --id=GitHub.GitHubDesktop -e;
+winget install --id=lukehaas.RunJS -e;
+winget install --id=Microsoft.AzureCLI -e;
+winget install --id=Microsoft.AzureFunctionsCoreTools -e;
+winget install --id=Microsoft.SQLServerManagementStudio -e;
+winget install --id=Microsoft.Teams -e;
+winget install --id=Miro.Miro -e;
+winget install --id=Postman.Postman -e;
+winget install --id=SublimeHQ.SublimeText.4 -e;
+
+
+# Tools
+#
+
+winget install --id=AutoHotkey.AutoHotkey -e;
+winget install --id=DebaucheeOpenSourceGroup.Barrier -e;
+winget install --id=File-New-Project.EarTrumpet -e;
+winget install --id=Microsoft.PowerShell -e;
+winget install --id=NickeManarin.ScreenToGif -e;
+winget install --id=OBSProject.OBSStudio -e;
+winget install --id=RARLab.WinRAR -e;
+winget install --id=WiresharkFoundation.Wireshark -e;
+
+
+# Redistributables
+#
+
+winget install --id=Microsoft.DirectX  -e;
+winget install --id=Microsoft.VCRedist.2005.x64 -e;
+winget install --id=Microsoft.VCRedist.2005.x86 -e;
+winget install --id=Microsoft.VCRedist.2008.x64 -e;
+winget install --id=Microsoft.VCRedist.2008.x86 -e;
+winget install --id=Microsoft.VCRedist.2013.x64 -e;
+winget install --id=Microsoft.VCRedist.2013.x86 -e;
+winget install --id=Microsoft.VCRedist.2015+.x64 -e;
+winget install --id=Microsoft.VCRedist.2015+.x86 -e;
