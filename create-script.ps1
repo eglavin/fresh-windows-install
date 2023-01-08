c9c5a750113099b126d715859e49a892a8bf2d39
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2023-01-08 18:39:27 +0000

    add create install script

---
+++ create-script.ps1
@@ -0,0 +1,131 @@
+# Script to product a list of `winget` install commands for a given list of packages
+#
+
+param (
+  [switch]$ShowOutput
+)
+
+
+$redistributes = @(
+  'Microsoft.DirectX ',
+  'Microsoft.VCRedist.2005.x86',
+  'Microsoft.VCRedist.2005.x64',
+  'Microsoft.VCRedist.2008.x86',
+  'Microsoft.VCRedist.2008.x64',
+  'Microsoft.VCRedist.2013.x86',
+  'Microsoft.VCRedist.2013.x64',
+  'Microsoft.VCRedist.2015+.x86',
+  'Microsoft.VCRedist.2015+.x64'
+);
+
+$info = @(
+  'ALCPU.CoreTemp',
+  'CPUID.CPU-Z',
+  'CPUID.HWMonitor',
+  'CrystalDewWorld.CrystalDiskInfo',
+  'CrystalDewWorld.CrystalDiskMark',
+  'TechPowerUp.GPU-Z'
+);
+
+$tools = @(
+  '7zip.7zip',
+  'Armin2208.WindowsAutoNightMode',
+  'Audacity.Audacity',
+  'AutoHotkey.AutoHotkey',
+  'BlenderFoundation.Blender',
+  'CodecGuide.K-LiteCodecPack.Standard',
+  'DebaucheeOpenSourceGroup.Barrier',
+  'File-New-Project.EarTrumpet',
+  'GIMP.GIMP',
+  'Logitech.GHUB',
+  'Microsoft.PowerShell',
+  'NickeManarin.ScreenToGif',
+  'Notepad++.Notepad++',
+  'OBSProject.OBSStudio',
+  'PuTTY.PuTTY',
+  'QL-Win.QuickLook',
+  'RARLab.WinRAR',
+  'RealVNC.VNCServer',
+  'RealVNC.VNCViewer',
+  'TheDocumentFoundation.LibreOffice',
+  'voidtools.Everything',
+  'WinDirStat.WinDirStat',
+  'WiresharkFoundation.Wireshark'
+);
+
+$security = @(
+  'Bitwarden.Bitwarden',
+  'Malwarebytes.Malwarebytes',
+  'NordVPN.NordVPN',
+  'Twilio.Authy'
+);
+
+$development = @(
+  'AdoptOpenJDK.OpenJDK.17',
+  'Atlassian.Sourcetree',
+  'CoreyButler.NVMforWindows',
+  'Figma.Figma',
+  'GitHub.GitHubDesktop',
+  'Google.AndroidStudio',
+  'Microsoft.AzureCLI',
+  'Microsoft.AzureFunctionsCoreTools',
+  'Microsoft.AzureStorageExplorer',
+  'Microsoft.SQLServerManagementStudio',
+  'Microsoft.Teams',
+  'Miro.Miro',
+  'Postman.Postman',
+  'SublimeHQ.SublimeText.4'
+);
+
+$browsers = @(
+  'Google.Chrome.Dev',
+  'Google.Chrome',
+  'Microsoft.Edge.Dev',
+  'Mozilla.Firefox.DeveloperEdition',
+  'Mozilla.Firefox'
+);
+
+$entertainment = @(
+  'Plex.Plex',
+  'Sky.SkyGo',
+  'Spotify.Spotify',
+  'VideoLAN.VLC'
+);
+
+
+# Create array of arrays to hold all lists and their names
+$allLists = $(
+  $('Redistributes', $redistributes),
+  $('Info', $info),
+  $('Tools', $tools),
+  $('Security', $security),
+  $('Development', $development),
+  $('Browsers', $browsers),
+  $('Entertainment', $entertainment)
+);
+
+
+# Create dynamic array to hold output
+# See: https://stackoverflow.com/a/33156229
+$output = New-Object System.Collections.Generic.List[System.Object];
+
+ForEach ($list in $allLists) {
+  $output.Add("# $($list[0])");
+
+  ForEach ($app in $list[1]) {
+    $output.Add("winget install --id=$app -e;");
+  }
+
+  $output.Add("");
+}
+
+
+# Output to console if requested
+if ($ShowOutput) {
+  ForEach ($line in $output) {
+    Write-Host $line;
+  }
+}
+
+# Create file with output
+$output | Out-File -FilePath .\install.ps1
---
+++ install.ps1
@@ -0,0 +1,79 @@
+# Redistributes
+winget install --id=Microsoft.DirectX  -e;
+winget install --id=Microsoft.VCRedist.2005.x86 -e;
+winget install --id=Microsoft.VCRedist.2005.x64 -e;
+winget install --id=Microsoft.VCRedist.2008.x86 -e;
+winget install --id=Microsoft.VCRedist.2008.x64 -e;
+winget install --id=Microsoft.VCRedist.2013.x86 -e;
+winget install --id=Microsoft.VCRedist.2013.x64 -e;
+winget install --id=Microsoft.VCRedist.2015+.x86 -e;
+winget install --id=Microsoft.VCRedist.2015+.x64 -e;
+
+# Info
+winget install --id=ALCPU.CoreTemp -e;
+winget install --id=CPUID.CPU-Z -e;
+winget install --id=CPUID.HWMonitor -e;
+winget install --id=CrystalDewWorld.CrystalDiskInfo -e;
+winget install --id=CrystalDewWorld.CrystalDiskMark -e;
+winget install --id=TechPowerUp.GPU-Z -e;
+
+# Tools
+winget install --id=7zip.7zip -e;
+winget install --id=Armin2208.WindowsAutoNightMode -e;
+winget install --id=Audacity.Audacity -e;
+winget install --id=AutoHotkey.AutoHotkey -e;
+winget install --id=BlenderFoundation.Blender -e;
+winget install --id=CodecGuide.K-LiteCodecPack.Standard -e;
+winget install --id=DebaucheeOpenSourceGroup.Barrier -e;
+winget install --id=File-New-Project.EarTrumpet -e;
+winget install --id=GIMP.GIMP -e;
+winget install --id=Logitech.GHUB -e;
+winget install --id=Microsoft.PowerShell -e;
+winget install --id=NickeManarin.ScreenToGif -e;
+winget install --id=Notepad++.Notepad++ -e;
+winget install --id=OBSProject.OBSStudio -e;
+winget install --id=PuTTY.PuTTY -e;
+winget install --id=QL-Win.QuickLook -e;
+winget install --id=RARLab.WinRAR -e;
+winget install --id=RealVNC.VNCServer -e;
+winget install --id=RealVNC.VNCViewer -e;
+winget install --id=TheDocumentFoundation.LibreOffice -e;
+winget install --id=voidtools.Everything -e;
+winget install --id=WinDirStat.WinDirStat -e;
+winget install --id=WiresharkFoundation.Wireshark -e;
+
+# Security
+winget install --id=Bitwarden.Bitwarden -e;
+winget install --id=Malwarebytes.Malwarebytes -e;
+winget install --id=NordVPN.NordVPN -e;
+winget install --id=Twilio.Authy -e;
+
+# Development
+winget install --id=AdoptOpenJDK.OpenJDK.17 -e;
+winget install --id=Atlassian.Sourcetree -e;
+winget install --id=CoreyButler.NVMforWindows -e;
+winget install --id=Figma.Figma -e;
+winget install --id=GitHub.GitHubDesktop -e;
+winget install --id=Google.AndroidStudio -e;
+winget install --id=Microsoft.AzureCLI -e;
+winget install --id=Microsoft.AzureFunctionsCoreTools -e;
+winget install --id=Microsoft.AzureStorageExplorer -e;
+winget install --id=Microsoft.SQLServerManagementStudio -e;
+winget install --id=Microsoft.Teams -e;
+winget install --id=Miro.Miro -e;
+winget install --id=Postman.Postman -e;
+winget install --id=SublimeHQ.SublimeText.4 -e;
+
+# Browsers
+winget install --id=Google.Chrome.Dev -e;
+winget install --id=Google.Chrome -e;
+winget install --id=Microsoft.Edge.Dev -e;
+winget install --id=Mozilla.Firefox.DeveloperEdition -e;
+winget install --id=Mozilla.Firefox -e;
+
+# Entertainment
+winget install --id=Plex.Plex -e;
+winget install --id=Sky.SkyGo -e;
+winget install --id=Spotify.Spotify -e;
+winget install --id=VideoLAN.VLC -e;
+
